58a31f503bc5da8968be42059b91f4804018ae3e
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-05-01 23:46:09 +0700

    Add knexfile config
    knexfile config js

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+};
