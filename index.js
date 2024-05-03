3a58d651b7108ee09356f5eb078e9447450494c7
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-05-03 16:46:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const db = require('./db/func/pool');
 
 const app = express();
 
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // SwaggerUI Docs
 // Please Add $DOCS_URL and $DOCS_PORT for HTTP or $DOCS_HTTPS_URL for HTTPS to build ENV
 const fallbackURL = 'http://localhost:8080';
@@ -40,27 +45,13 @@ app.use(
   swaggerUi.setup(swSpecs),
 );
 
-// const db = new Pool({
-//   user: process.env.PG_USER,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   password: process.env.PG_PASSWORD,
-//   port: process.env.PG_PORT,
-// });
-
-// Connect to the database
+// DB Check
 db.connect()
   .then(() => console.log('Connected to PostgreSQL'))
   .catch((err) => console.error('Error connecting to PostgreSQL:', err));
 
-// Middleware to handle database connection
-app.use((req, res, next) => {
-  req.db = db;
-  next();
-});
-
 // Define Routes
-app.use('auth/v1', require('./routes/auth'));
+app.use('/auth/v1', require('./routes/auth'));
 
 // Start the server
 const port = process.env.API_PORT || 8080;
