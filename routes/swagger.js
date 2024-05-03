de8659466eee26ab86968640893c1283fbd470f8
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-05-03 15:44:08 +0700

    Create swagger.js: Swagger Config
    Create swagger.js: Swagger Config

---
+++ routes/swagger.js
@@ -0,0 +1,26 @@
+/**
+ *  @swagger
+ *  components:
+ *    securitySchemes:
+ *      bearerAuth:
+ *        type: http
+ *        scheme: bearer
+ */
+
+/**
+ *  @swagger
+ *  components:
+ *    definitions-response:
+ *      regularResponse:
+ *        type: object
+ *        properties:
+ *          error:
+ *            type: boolean
+ *            required: true
+ *          message:
+ *            type: string
+ *            required: true
+ *          details:
+ *            type: string
+ *            required: false
+ */
