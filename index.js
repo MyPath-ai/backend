b2e787648059841cc61d4f78b432ced6e0241246
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-05-03 15:43:24 +0700

    Update index.js: Removing function, using require instead

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 require('dotenv').config();
 const express = require('express');
-const { Pool } = require('pg');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const db = require('./db/func/pool');
 
 const app = express();
 
-const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
+// SwaggerUI Docs
+// Please Add $DOCS_URL and $DOCS_PORT for HTTP or $DOCS_HTTPS_URL for HTTPS to build ENV
+const fallbackURL = 'http://localhost:8080';
+let envURL = null;
+if (process.env.DOCS_URL && process.env.DOCS_PORT) {
+  envURL = `http://${process.env.DOCS_URL}:${process.env.DOCS_PORT}`;
+} else if (process.env.DOCS_HTTPS_URL) {
+  envURL = `${process.env.DOCS_HTTPS_URL}`;
+}
+// const envURL = process.env.DOCS_URL && process.env.DOCS_PORT ? `http://${process.env.DOCS_URL}:${process.env.DOCS_PORT}` : `${process.env.DOCS_HTTPS_URL}`;
+const swOptions = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'MyPath-Ai API Documentation',
+    },
+    servers: [
+      {
+        url: envURL || fallbackURL,
+      },
+    ],
+  },
+  apis: [
+    './routes/*.js',
+  ],
+};
+
+const swSpecs = swaggerJsdoc(swOptions);
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swSpecs),
+);
+
+// const db = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
 
 // Connect to the database
 db.connect()
@@ -26,6 +60,7 @@ app.use((req, res, next) => {
 });
 
 // Define Routes
+app.use('auth/v1', require('./routes/auth'));
 
 // Start the server
 const port = process.env.API_PORT || 8080;
